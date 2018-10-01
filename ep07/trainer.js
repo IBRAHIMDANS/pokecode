16d3e2bc3427d72f797189ec9d4e40003e8f53fe
Student <Student@B15-10.local>
2018-10-01 10:30:50 +0200

    ep07

---
+++ ep07/trainer.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+
+class Trainer{
+
+  constructor(firstname, age ){
+    if (typeof Trainer.counter == 'undefined') {
+      Trainer.counter =1
+    }
+    this.firstname =firstname
+    this.age = age
+    this.id= Trainer.counter++
+    this.pokemons =[]
+
+    const d = new Date()
+    this.startDate = `${d.getFullYear()}-${d.getMonth() +1}-${d.getDate()}`
+    console.log(`Here come's a new challenger ••[[ ${this.id} ${this.firstname}`);
+  }
+
+  hey(){
+      const nbpokemon = this.pokemons.length
+      console.log(`
+  Yoooo!
+  I'm ${this.firstname} and I'm ${this.age} years old.
+  I have ${this.id} pokemon${this.id > 1 ? 's' : ''}and I will be the best virtual pokemon master.
+  `)
+      }
+    start(pokemons){
+      this.pokemons.push(pokemons[Math.floor(Math.random() * pokemons.length)]);
+      console.log(`Yeaaaaaaah, my first pokémon is ${this.pokemons[0].pokename}
+      `);
+      this.pokemons[0].yell()
+    }
+    serialize(){
+let pokemons =[]
+for (const p of this.pokemons) {
+  pokemons.push(p.number);
+}
+      let data = {
+        traner  : [
+{
+  id : this.id,
+firstname : this.firstname,
+age : this.age,
+pokemons,
+startDate : this.startDate
+          }
+        ]
+      }
+      fs.writeFileSync("pokestory.json",JSON.stringify(data,null, 3));
+    }
+}
+module.exports = Trainer
