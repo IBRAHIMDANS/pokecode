2cd4b7ecfdf9ffe0323a76ba103a07d3ceba7282
Student <Student@B15-10.local>
2018-10-02 14:28:14 +0200

    ok

---
+++ ep10/PokemonFactory.js
@@ -0,0 +1,31 @@
+const Pokemon = require('./Pokemon');
+const pokedex = require('../data/pokedex.json')
+
+class PokemonFactory {
+
+  static create({key, value}){
+    const pokemon = pokedex.find(function(item){
+      return item[key] == value;
+    });
+
+    let type = []
+    let i =1
+    while (pokemon[`type${i}`] != undefined) {
+      type.push(pokemon[`type${i++}`]);
+    }
+    let attacks =[]
+    for (let attack of pokemon.attaques) {
+      attacks.push(attack);
+    }
+    let pokedata = {
+      number : pokemon.ndex,
+      name : pokemon.nom,
+      size : pokemon.taille,
+      weight : pokemon.poids,
+      type,
+      attacks
+    };
+    return new Pokemon(pokedata);
+  }
+}
+module.exports = PokemonFactory
