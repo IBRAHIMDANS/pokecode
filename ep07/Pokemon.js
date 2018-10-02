1cf0cef24aa2c3987696e7b980e76b64c7a157c3
Student <Student@B15-10.local>
2018-10-02 10:11:56 +0200

    ep08

---
+++ ep07/Pokemon.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const Trainer = require('./trainer');
+const pokedex = require('../data/pokedex.json')
+
+
+
+class Pokemon {
+
+
+  constructor ({number,name,size,weight,type,level=3}, is_yelling = false){
+    this.number = number
+    this.pokename = name
+    this.size = size
+    this.weight = weight
+    this.type = type
+    this.level = level
+    if(is_yelling){
+    this.yell()
+    }
+  }
+
+  yell(){
+    console.log(`•••••• ${this.pokename.toUpperCase()}`);
+  }
+
+  dump(on_file= false){
+    let type_str= '[';
+    for (const t of this.type){
+      type_str += `'${t}']`;
+    }
+    let output = `
+    ••••••#${this.number} ${this.pokename}
+    size >> ${this.size}, weight >> ${this.weight}, type >> ${type_str}
+    `;
+
+  if (on_file) {
+    const d = new Date();
+    const  formatDate =  `${d.getFullYear()}-${d.getMonth()+ 1}-${d.getDate()}`;
+    const logname = `pokedecode.${formatDate}.log`;
+    fs.writeFileSync(logname, output, "utf-8");
+    console.log(`# The dump is successfully saved on file ${logname}`);
+
+
+
+  }
+  else
+  {
+    console.log(output);
+    }
+  }
+}
+module.exports = Pokemon
---
+++ ep08/Game.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const Trainer = require('./trainer');
+const Pokemon = require('./Pokemon');
+const PokemonFactory = require('./PokemonFactory');
+const pokedex = require('../data/pokedex.json')
+
+class Game {
+ init()
+
+
+}
