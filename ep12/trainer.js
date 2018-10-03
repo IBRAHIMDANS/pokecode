9f9166e1d0738c72ecfc3a812cd96b66b8f4be77
Student <Student@B15-10.local>
2018-10-03 14:44:18 +0200

    ok

--- ep12/trainer.js
+++ ep12/trainer.js
@@ -14,50 +14,50 @@ class Trainer{
     const d = new Date()
     this.startDate = `${d.getFullYear()}-${d.getMonth() +1}-${d.getDate()}`
     console.log(`Here come's a new challenger ••[[ ${this.id} ${this.firstname}`);
-  }
+    }
 
-  hey(){
+    hey(){
       const nbpokemon = this.pokemons.length
       console.log(`
-  Yoooo!
-  I'm ${this.firstname} and I'm ${this.age} years old.
-  I have ${this.id} pokemon${this.id > 1 ? 's' : ''}and I will be the best virtual pokemon master.
-  `)
+        Yoooo!
+        I'm ${this.firstname} and I'm ${this.age} years old.
+        I have ${this.id} pokemon${this.id > 1 ? 's' : ''}and I will be the best virtual pokemon master.
+        `)
       }
-    start(pokemons){
-      this.pokemons.push(pokemons[Math.floor(Math.random() * pokemons.length)]);
-      console.log(`Yeaaaaaaah, my first pokémon is ${this.pokemons[0].pokename}
-      `);
-      this.pokemons[0].yell()
-    }
-    getPokemons(pokemon){
-      return this.pokemons
-    }
-    setPokemons(pokemon){
-      this.pokemons = (pokemon);
-    }
-    list(){
-      for (let pokemon of this.pokemons) {
-      pokemon.yell();
-      }
-    }
-    serialize(){
-let pokemons =[]
-for (const p of this.pokemons) {
-  pokemons.push(p.number);
-}
-      let data = {
-        trainers  : [
-{
-  id : this.id,
-firstname : this.firstname,
-age : this.age,
-pokemons,
-startDate : this.startDate
+      start(pokemons){
+        this.pokemons.push(pokemons[Math.floor(Math.random() * pokemons.length)]);
+        console.log(`Yeaaaaaaah, my first pokémon is ${this.pokemons[0].pokename}
+          `);
+          this.pokemons[0].yell()
+        }
+        getPokemons(pokemon){
+          return this.pokemons
+        }
+        setPokemons(pokemon){
+          this.pokemons = (pokemon);
+        }
+        list(){
+          for (let pokemon of this.pokemons) {
+            pokemon.yell();
           }
-        ]
+        }
+        serialize(){
+          let pokemons =[]
+          for (const p of this.pokemons) {
+            pokemons.push(p.number);
+          }
+          let data = {
+            trainers  : [
+              {
+                id : this.id,
+                firstname : this.firstname,
+                age : this.age,
+                pokemons,
+                startDate : this.startDate
+              }
+            ]
+          }
+          fs.writeFileSync("pokestory.json",JSON.stringify(data,null, 3));
+        }
       }
-      fs.writeFileSync("pokestory.json",JSON.stringify(data,null, 3));
-    }
-}
-module.exports = Trainer
+      module.exports = Trainer
